faddf113e6481d6a9b40fd1174559d5ee92b61ba
Flavio Copes <copesc@gmail.com>
2019-01-19 12:05:57 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import Button from './components/Button'
+
+import './styles.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const incrementCount = increment => {
+    setCount(count + increment)
+  }
+
+  return (
+    <div className="App">
+      <Button increment={1} onClickFunction={incrementCount} />
+      <Button increment={10} onClickFunction={incrementCount} />
+      <Button increment={100} onClickFunction={incrementCount} />
+      <Button increment={1000} onClickFunction={incrementCount} />
+      <span>{count}</span>
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
